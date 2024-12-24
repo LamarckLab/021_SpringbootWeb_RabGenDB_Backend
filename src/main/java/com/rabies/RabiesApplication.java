7891d72d74d1433bdd4f50eaf89b12df932223e8
Lamarck <704021302@qq.com>
2024-12-24 15:05:07 +0800

    RabGenDB后端第一次提交

---
+++ src/main/java/com/rabies/RabiesApplication.java
@@ -0,0 +1,13 @@
+package com.rabies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabiesApplication.class, args);
+    }
+
+}
