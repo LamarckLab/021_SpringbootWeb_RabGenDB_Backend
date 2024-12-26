73a40afd61ebd85f8630121b2317c3611e2bb999
Lamarck <704021302@qq.com>
2024-12-26 16:59:41 +0800

    把Mapper层的内容写完了

--- src/main/java/com/rabies/mapper/GenomeMapper.java
+++ src/main/java/com/rabies/mapper/GenomeMapper.java
@@ -9,7 +9,27 @@ import java.util.List;
 @Mapper
 public interface GenomeMapper {
 
-
     @Select("select * from rabies.genome")
     List<Genome> listGenomes();
+
+    @Select("select * from rabies.genome where accession =#{accession}")
+    List<Genome> findByAccession(String accession);
+
+    @Select("select * from rabies.genome where collectionCountry =#{country}")
+    List<Genome> findByCountry(String country);
+
+    @Select("select * from rabies.genome where refinedHost =#{refinedHost}")
+    List<Genome> findByHost(String refinedHost);
+
+    @Select("select * from rabies.genome where accession =#{accession} and collectionCountry =#{country}")
+    List<Genome> findByAccessionAndCountry(String accession, String country);
+
+    @Select("select * from rabies.genome where accession =#{accession} and refinedHost =#{refinedHost}")
+    List<Genome> findByAccessionAndHost(String accession, String refinedHost);
+
+    @Select("select * from rabies.genome where collectionCountry =#{country} and refinedHost =#{refinedHost}")
+    List<Genome> findByCounrtyAndHost(String country, String refinedHost);
+
+    @Select("select * from rabies.genome where accession =#{accession} and collectionCountry = #{country} and refinedHost =#{refinedHost}")
+    List<Genome> findByAccessionAndCountryAndHost(String accession, String country, String refinedHost);
 }
